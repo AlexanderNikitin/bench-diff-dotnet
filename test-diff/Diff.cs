8bc9b04da1a15b9286b1dd839d3d9de567de7890
Alexander Nikitin <alexnik000@yandex.ru>
2023-02-26 07:25:53 +0300

    Safe

--- test-diff/Diff.cs
+++ test-diff/Diff.cs
@@ -9,70 +9,68 @@
         }
 
         public int[][] Compare(SequencePair sequencePair) {
-            unsafe {
-                int n = sequencePair.Length1 + 1;
-                int m = sequencePair.Length2 + 1;
+            int n = sequencePair.Length1 + 1;
+            int m = sequencePair.Length2 + 1;
 
-                CacheHolder[] prevCacheLine = new CacheHolder[m];
-                CacheHolder[] currentCacheLine = new CacheHolder[m];
+            CacheHolder[] prevCacheLine = new CacheHolder[m];
+            CacheHolder[] currentCacheLine = new CacheHolder[m];
 
-                CacheHolder[][] cache = {
+            CacheHolder[][] cache = {
                     prevCacheLine,
                     currentCacheLine
                 };
-                Array.Fill(cache[0], NULL);
-                cache[1][0] = NULL;
+            Array.Fill(cache[0], NULL);
+            cache[1][0] = NULL;
 
-                for (int i = 1, prevI = 0; i < n; prevI = i++) {
-                    CacheHolder curIPrevJ = currentCacheLine[0];
-                    for (int j = 1, prevJ = 0; j < m; prevJ = j++) {
-                        CacheHolder res;
-                        if (sequencePair.Equal(prevI, prevJ)) {
-                            CacheHolder prevIPrevJ = prevCacheLine[prevJ];
-                            res = new CacheHolder(prevIPrevJ.Count + 1, C, prevIPrevJ);
-                        } else {
-                             CacheHolder prevICurJ = prevCacheLine[j];
-                             int a = prevICurJ.Count;
-                             int b = curIPrevJ.Count;
-                            res = a > b || a == b && i < j ?
-                                    new CacheHolder(a, A, prevICurJ) :
-                                    new CacheHolder(b, B, curIPrevJ);
-                        }
-
-                        currentCacheLine[j] = res;
-                        curIPrevJ = res;
+            for (int i = 1, prevI = 0; i < n; prevI = i++) {
+                CacheHolder curIPrevJ = currentCacheLine[0];
+                for (int j = 1, prevJ = 0; j < m; prevJ = j++) {
+                    CacheHolder res;
+                    if (sequencePair.Equal(prevI, prevJ)) {
+                        CacheHolder prevIPrevJ = prevCacheLine[prevJ];
+                        res = new CacheHolder(prevIPrevJ.Count + 1, C, prevIPrevJ);
+                    } else {
+                        CacheHolder prevICurJ = prevCacheLine[j];
+                        int a = prevICurJ.Count;
+                        int b = curIPrevJ.Count;
+                        res = a > b || a == b && i < j ?
+                                new CacheHolder(a, A, prevICurJ) :
+                                new CacheHolder(b, B, curIPrevJ);
                     }
 
-                    CacheHolder[] temp = prevCacheLine;
-                    prevCacheLine = currentCacheLine;
-                    currentCacheLine = temp;
+                    currentCacheLine[j] = res;
+                    curIPrevJ = res;
                 }
 
-                int[][] result = new int[Math.Min(sequencePair.Length1, sequencePair.Length2)][];
-                int index = result.Length;
-                CacheHolder cur = cache[sequencePair.Length1 % 2][sequencePair.Length2];
-                for (int i = sequencePair.Length1, j = sequencePair.Length2;
-                     i > 0 && j > 0;) {
-                    switch (cur.Side) {
-                        case C:
-                            result[--index] = new int[] { --i, --j };
-                            break;
-                        case A:
-                            i--;
-                            break;
-                        case B:
-                            j--;
-                            break;
-                    }
-                    cur = cur.Prev;
-                }
-                if (index > 0) {
-                    int[][] temp = new int[result.Length - index][];
-                    Array.Copy(result, index, temp, 0, result.Length - index);
-                    result = temp;
-                }
-                return result;
+                CacheHolder[] temp = prevCacheLine;
+                prevCacheLine = currentCacheLine;
+                currentCacheLine = temp;
             }
+
+            int[][] result = new int[Math.Min(sequencePair.Length1, sequencePair.Length2)][];
+            int index = result.Length;
+            CacheHolder cur = cache[sequencePair.Length1 % 2][sequencePair.Length2];
+            for (int i = sequencePair.Length1, j = sequencePair.Length2;
+                 i > 0 && j > 0;) {
+                switch (cur.Side) {
+                    case C:
+                        result[--index] = new int[] { --i, --j };
+                        break;
+                    case A:
+                        i--;
+                        break;
+                    case B:
+                        j--;
+                        break;
+                }
+                cur = cur.Prev;
+            }
+            if (index > 0) {
+                int[][] temp = new int[result.Length - index][];
+                Array.Copy(result, index, temp, 0, result.Length - index);
+                result = temp;
+            }
+            return result;
         }
         private record CacheHolder(int Count, int Side, CacheHolder? Prev) {
         }
