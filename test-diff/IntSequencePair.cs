42cf11693975908521f0f317b9ad1c94f9f9c97a
AlexanderNikitin <alexnik000@yandex.ru>
2023-03-05 12:21:54 +0300

    Safe.

--- test-diff/IntSequencePair.cs
+++ test-diff/IntSequencePair.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 namespace test_diff
 {
-    public class IntSequencePair:AbstractSequencePair<int[]>
+    public class IntSequencePair : AbstractSequencePair<int[]>
     {
-        public IntSequencePair(int[] s1, int[] s2):base(s1, s2)
+        public IntSequencePair(int[] s1, int[] s2) : base(s1, s2)
         {
         }
 
         public override bool Equal(int index1, int index2)
         {
-            unsafe {
-                return s1[index1] == s2[index2];
-            }
+            return s1[index1] == s2[index2];
         }
 
         protected override int GetLength(int[] s)
