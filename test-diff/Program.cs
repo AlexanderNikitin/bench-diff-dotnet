e4a6c7a7457f60e2b5c6f603a97495658a9cc351
AlexanderNikitin <alexnik000@yandex.ru>
2023-03-05 11:40:53 +0300

    Exclude data generation

--- test-diff/Program.cs
+++ test-diff/Program.cs
@@ -1,26 +1,38 @@
 ﻿using System.Diagnostics;
 using test_diff;
 
-
-
-const int LENGTH = 10000;
-int[] a1 = new int[LENGTH];
-int[] a2 = new int[LENGTH];
 Random random = new Random();
 int[][]? values = null;
-Stopwatch stopWatch = new Stopwatch();
-stopWatch.Start();
-for (int i = 0; i < 20; i++) {
-    for (int j = 0; j < LENGTH; j++) {
-        a1[j] = random.Next(LENGTH / 100);
-        a2[j] = random.Next(LENGTH / 100);
-    }
+Stopwatch stopWatch = Stopwatch.StartNew();
+foreach(var kvp in dataGen(10000, 100, 20))
+{
     IDiff<int> diff = new Diff<int>();
-    int[][] temp = diff.Compare(new IntSequencePair(a1, a2));
-    if (values == null || temp.Length > values.Length) {
+    int[][] temp = diff.Compare(new IntSequencePair(kvp.Value, kvp.Key));
+    if (values == null || temp.Length > values.Length)
+    {
         values = temp;
     }
 }
 stopWatch.Stop();
 Console.WriteLine(values?.Length);
 Console.WriteLine(stopWatch.ElapsedMilliseconds);
+Console.ReadKey();
+
+
+IEnumerable<KeyValuePair<int[], int[]>> dataGen(int length, int ratio, int iterations)
+{
+    IList<KeyValuePair<int[], int[]>> result = new List<KeyValuePair<int[], int[]>>();
+    int maxRandom = length / ratio;
+    for(int i = 0; i < iterations; i++)
+    {
+        int[] s1 = new int[length];
+        int[] s2 = new int[length];
+        result.Add(new KeyValuePair<int[], int[]>(s1, s2));
+        for(int j = 0; j < length;j++)
+        {
+            s1[j] = random.Next(maxRandom);
+            s2[j] = random.Next(maxRandom);
+        }
+    }
+    return result;
+}
